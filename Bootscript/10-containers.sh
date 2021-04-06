bd860c4d3dd417eea02b51c6d28e913fdd8b863d
renedis <renedis@hotmail.com>
2021-04-06 13:12:37 +0200

    deleted mariadb

--- Bootscript/10-containers.sh
+++ Bootscript/10-containers.sh
@@ -5,7 +5,6 @@ VLAN=5
 IPV4_IP_NETDATA="10.0.5.4"
 IPV4_IP_GLANCES="10.0.5.5"
 IPV4_IP_PROXYMANAGER="10.0.5.6"
-IPV4_IP_MARIADB="10.0.5.7"
 
 # This is the IP address of the container. You may want to set it to match
 # your own network structure such as 192.168.5.3 or similar.
@@ -17,7 +16,6 @@ IPV4_GW="10.0.5.1/24"
 CONTAINER_NETDATA=netdata
 CONTAINER_GLANCES=glances
 CONTAINER_PROXYMANAGER=proxymanager
-CONTAINER_MARIADB=mariadb
 
 ## network configuration and startup:
 CNI_PATH=/mnt/data/podman/cni
@@ -58,9 +56,7 @@ ip route add ${IPV4_IP_GLANCES}/32 dev br${VLAN}.mac
 # add IPv4 route to DNS container
 ip route add ${IPV4_IP_PROXYMANAGER}/32 dev br${VLAN}.mac
 #######################################################################################
-# add IPv4 route to DNS container
-ip route add ${IPV4_IP_MARIADB}/32 dev br${VLAN}.mac
-#######################################################################################
+
 
 #######################################################################################
 if podman container exists ${CONTAINER_NETDATA}; then
@@ -79,10 +75,4 @@ if podman container exists ${CONTAINER_PROXYMANAGER}; then
   podman start ${CONTAINER_PROXYMANAGER}
 else
   logger -s -t podman-dns -p ERROR Container $CONTAINER_PROXYMANAGER not found, make sure you set the proper name, you can ignore this error if it is your first time setting it up
-fi
-#######################################################################################
-if podman container exists ${CONTAINER_MARIADB}; then
-  podman start ${CONTAINER_MARIADB}
-else
-  logger -s -t podman-dns -p ERROR Container $CONTAINER_MARIADB not found, make sure you set the proper name, you can ignore this error if it is your first time setting it up
 fi
